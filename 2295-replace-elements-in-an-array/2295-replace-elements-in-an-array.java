9740a3d4f469e7943ed16363e83f1b9e7809d3e5
Satyam Singh <satyamsingh9e@gmail.com>
2026-07-06 16:47:15 +0530

    Time: 49 ms (22.31%), Space: 142.8 MB (68.46%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] arrayChange(int[] nums, int[][] operations) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int[] op : operations) {
+
+            int oldVal = op[0];
+            int newVal = op[1];
+
+            int index = map.get(oldVal);
+
+            nums[index] = newVal;
+
+            map.remove(oldVal);
+            map.put(newVal, index);
+        }
+
+        return nums;
+    }
+}
