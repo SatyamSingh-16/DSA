a1259a8d2e9c075618d3265d88af222283b1a911
Satyam Singh <satyamsingh9e@gmail.com>
2026-08-01 08:39:30 +0530

    Time: 34 ms (60.99%), Space: 58.3 MB (94.7%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,25 +3,20 @@ class Solution {
         List<List<Integer>> arr = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1])
+        for(int i = 0;i<nums.length-2;i++){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
-
-            int l = i + 1;
-            int r = nums.length - 1;
-
-            while (l < r) {
-
+            }
+            int l = i+1;
+            int r = nums.length-1;
+            
+            while(l<r){
                 int sum = nums[i] + nums[l] + nums[r];
 
-                if (sum == 0) {
-
-                    arr.add(Arrays.asList(nums[i], nums[l], nums[r]));
-
+                if(sum == 0){
+                    arr.add(Arrays.asList(nums[i],nums[l],nums[r]));
                     l++;
                     r--;
-
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
                     }
@@ -29,15 +24,14 @@ class Solution {
                     while (l < r && nums[r] == nums[r + 1]) {
                         r--;
                     }
-
-                } else if (sum < 0) {
+                }else if(sum <0){
                     l++;
-                } else {
+                }else{
                     r--;
                 }
+                
             }
         }
-
         return arr;
     }
 }
